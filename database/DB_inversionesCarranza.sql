d5b8f6699d74e154e87afb1f041a8a16df685694
OmarCarranza15 <79736446+OmarCarranza15@users.noreply.github.com>
2024-02-06 15:25:47 -0600

    Update DB_inversionesCarranza.sql

--- database/DB_inversionesCarranza.sql
+++ database/DB_inversionesCarranza.sql
@@ -3,7 +3,7 @@ use db_incarranza;
 
 create table Usuarios (
 	id int not null auto_increment primary key,
-    rol int not null,
+    rol int not null, /* El rol nos sirve para identificar si es admin o cliente, para esto usaremos lo siguente 1 = admin y 2 = cliente*/
     nombre varchar(45) not null,
     apellido varchar(45) not null,
     correo varchar(100) not null,
@@ -13,3 +13,4 @@ create table Usuarios (
     createAt datetime DEFAULT CURRENT_TIMESTAMP,
     updateAt datetime DEFAULT CURRENT_TIMESTAMP
 )
+
