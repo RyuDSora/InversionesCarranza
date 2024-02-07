436a5a562821612570aab1b1141bdfdfb9ae7abe
OmarCarranza15 <79736446+OmarCarranza15@users.noreply.github.com>
2024-02-07 16:12:32 -0600

    conexion de base de datos

---
+++ backend/database/db.js
@@ -0,0 +1,20 @@
+import {Sequelize} from 'sequelize'
+    const db = new Sequelize('db_incarranza', 'root', '1234',{
+        host: 'localhost',                          //contraseña de mySQL
+        dialect: 'mysql'
+    })
+
+export default db
+
+//intalar las siguentes dependencia 
+/*npm install sequelize
+   */
+
+try {
+    await db.authenticate();
+    console.log('Conexion exitosa a la DB');
+    } catch (error) {
+    console.log(`El error de conexion es: ${error}`);
+}
+
+//nodemon db, este comando nos sirve para verificar si la base de datos tiene una conexion exitosa 
