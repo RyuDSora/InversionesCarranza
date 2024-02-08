3e7c6935e60a4c3ef44c93aa25215e0a36d9810b
AnibalGodinez <anibalgodinez64@gmail.com>
2024-02-08 16:39:32 -0600

    add: Formulario

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
+import React, { useState } from 'react';
 import './App.css';
 import Barra  from "./Barra";
 import Cuerpo from './Cuerpo'
 import PiePag from './PiePag'
-
+// aqui es mejor utilizar etiquetas de react para llamar a los componentes
 function App() {
+  const [page, setPage1] = useState("landing") // [estado, funcion que modifica el estado]
+  const setPage = (page) => {
+    setPage1(page)
+  }
   return (
     <div className="App">
-      <header className="">
-        {Barra()}
+      <header>
+        <Barra setPage={setPage}/>
       </header>
       <div>
-        {Cuerpo()}
+        <Cuerpo page={page}/>
       </div>
       {PiePag()}
     </div>
---
+++ frontend/src/componets/Signup.jsx
@@ -0,0 +1,80 @@
+import "../styles/Signup.css";
+export default function Signup() {
+  return (
+    <form>
+      <div className="form-title"><p>Registrarse</p></div>
+      <section className="form-group">
+        <label htmlFor="name-user">
+          Nombres:
+        </label>
+          <input
+            title="Ingrese sus nombres"
+            type="text"
+            className="form-control"
+            placeholder="Nombres del usuario"
+            id="name-user"
+          />
+      </section>
+      <section className="form-group">
+        <label htmlFor="name-user2">
+          Apellidos:
+        </label>
+          <input
+            title="Ingrese sus apellidos"
+            type="text"
+            className="form-control"
+            placeholder="Nombres del usuario"
+            id="name-user2"
+          />
+      </section>
+      <section className="form-group">
+        <label htmlFor="name-email">
+          Correo:
+        </label>
+          <input
+            title="Ingrese un correo electrónico"
+            type="email"
+            className="form-control"
+            placeholder="Correo electrónico del usuario"
+            id="name-email"
+          />
+      </section>
+      <section className="form-group">
+        <label htmlFor="name-phone">
+          Número de teléfono:
+        </label>
+          <input
+            title="Ingrese un número de teléfono"
+            type="tel"
+            className="form-control"
+            placeholder="Número de teléfono del usuario"
+            id="name-phone"
+          />
+      </section>
+      <section className="form-group">
+        <label htmlFor="name-password">
+          Contraseña:
+        </label>
+          <input
+            title="Ingrese una contraseña"
+            type="password"
+            className="form-control"
+            placeholder="Contraseña del usuario"
+            id="name-password"
+          />
+      </section>
+      <section className="form-group">
+        <label htmlFor="name-date">
+          Fecha de nacimiento:
+        </label>
+          <input
+            title="Ingrese su fecha de nacimiento"
+            type="date"
+            className="form-control"
+            placeholder="Fecha de nacimiento del usuario"
+            id="name-date"
+          />
+      </section>
+    </form>
+  );
+}
