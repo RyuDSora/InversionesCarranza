30f490f5c9ba0524578f2aa1f273ff4686193fe3
Ronny Salazar <ronnysalazar98@gmail.com>
2024-02-21 14:59:08 -0600

    Servicios 2.0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
 import "./App.css";
 import FooterIC from "./componets/FooterIC.jsx";
 import CompRegistro from "./componets/Signup.jsx";
@@ -11,10 +10,10 @@ import LandingPage from "./componets/LandingPage.jsx";
 import NavBarIC from "./componets/NavBariC.jsx";
 import RecuperarLanding from './componets/RecuperarLanding.jsx'
 import CambiarContrasenia from "./componets/CambiarContrasenia.jsx";
+import Servicios from './componets/Servicios.jsx'; 
 import Perfil from './componets/Perfil.jsx';
 import EditPerfil from './componets/EditarPerfil.jsx';
 import Projects from './componets/projects.jsx'
-
 
 function App() {
 
@@ -32,7 +31,7 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/Recuperar" element={<RecuperarLanding />} />
               <Route path="/CambiarContrasenia" element={<CambiarContrasenia/>} />
-              <Route path="/Servicios" element={ "Servicios" } />
+              <Route path="/Servicios" element={ <Servicios /> } />
               <Route path="/perfil/:userId" element={<Perfil />} />
               <Route path="/EditarPerfil/:userId" element = {<EditPerfil/>}/>
               <Route path="/Proyectos" element={ <Projects/> } />
--- frontend/src/componets/Servicios.jsx
+++ frontend/src/componets/Servicios.jsx
@@ -1,47 +1,54 @@
-import Container from 'react-bootstrap/esm/Container';
-import PR from '../imgs/CasaAthems.jpg';
-export default function Servicios(params) {
-    
-    return(
-        <Container>
-            <div className='bg-light border rounded-3'>
-                <div className='p-2 shadow'><span className='h2'>Nuestros Servicios</span></div><br />
+import React from 'react';
+import { Card, Container, Row, Col, Button } from 'react-bootstrap';
 
-                <p> A continuación se muestran los servicios que ofrece nuestra empresa</p>
-                
-                {Serv('Diseño','/Servicios/Diseño')}
-                <hr />
-                {Serv('Construcción','/Servicios/Construcción')}
-                <hr />
-                {Serv('Mantenimiento','/Servicios/Mantenimiento')}
-                <hr />
-                {Serv('Remodelación','/Servicios/Remodelación')}
+function Servicios() {
+  const servicios = [
+    { 
+      nombre: 'Construcción de Oficinas o Espacios de Trabajo', 
+      descripcion: 'Construimos oficinas de alta calidad y espacios de trabajo minimalistas.', 
+      imagen: 'https://scontent.ftgu4-1.fna.fbcdn.net/v/t39.30808-6/324567143_853556549283661_2032805455122645712_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=NDID2dzX9gcAX_qsAqJ&_nc_ht=scontent.ftgu4-1.fna&oh=00_AfCguxyZG5UYtlOoomQCtlgoB-CmjMPFoclBo2N-BSMLrg&oe=65DBFAE6', 
+      infoAdicional: 'Información adicional...' 
+    },
+    { 
+      nombre: 'Construcción de Casas', 
+      descripcion: 'Construimos la casa de tus sueños.', 
+      imagen: 'https://scontent.ftgu4-1.fna.fbcdn.net/v/t39.30808-6/322331734_1335197530666565_1245614110228181443_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=EBjzGQqZgFgAX9bdaHy&_nc_ht=scontent.ftgu4-1.fna&oh=00_AfA_TAuGztruciewHAs9YaA2H8hJ3cTyeDaWXPa9CG8OrA&oe=65DBD15A', 
+      infoAdicional: 'Información adicional...' 
+    },
+    { 
+      nombre: 'Diseño de Estructuras', 
+      descripcion: 'Diseñamos con los estilos más modernos y de alto rendimiento.', 
+      imagen: 'https://scontent.ftgu4-1.fna.fbcdn.net/v/t1.6435-9/102263905_174768493996723_5569305607189909180_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=dd63ad&_nc_ohc=gpLiD2NeggkAX8_SrLQ&_nc_ht=scontent.ftgu4-1.fna&oh=00_AfB-7ts0jGRS1v8_IV0s2r71F_d-ijTF_Aj-a_wY_Q_jww&oe=65FDD178', 
+      infoAdicional: 'Información adicional...' 
+    },
+    { 
+      nombre: 'Contratación de Arquitectos', 
+      descripcion: 'Contrata a los mejores Arquitectos de Honduras.', 
+      imagen: 'https://img.freepik.com/fotos-premium/concepto-construccion-estructura-reunion-ingenieros-o-arquitectos-proyectos-que-trabajan-socios-herramientas-ingenieria-construccion-modelos-planos-contrato-trabajo-ambas-empresas_265022-20316.jpg', 
+      infoAdicional: 'Información adicional...' 
+    },
+    // Agrega más servicios aquí...
+  ];
 
-            </div>
-        </Container>
-    );
-}
-function services(n,i,url) {
-    return(
-        <div style={{width:'300px'}} className='bg-light border rounded-3 pt-2 pb-3 my-2'>
-            <div className='p-2'><span className='h6'>{n}</span></div>
-            <div className='px-3'>
-                <img src={i} alt="img" className='w-100 border rounded-3'/>
-            </div><br />
-            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem suscipit, esse qui, ab sunt libero hic sint provident ipsam voluptates molestiae eligendi placeat cumque dolore sit rerum id sed consequuntur! </p>
-        </div>       
-    );
+  return (
+    <Container>
+      <Row>
+        {servicios.map((servicio, index) => (
+          <Col xs={12} sm={6} md={4} lg={3} key={index}>
+            <Card>
+              <Card.Img variant="top" src={servicio.imagen} />
+              <Card.Body>
+                <Card.Title>{servicio.nombre}</Card.Title>
+                <Card.Text>{servicio.descripcion}</Card.Text>
+                <Card.Text>{servicio.infoAdicional}</Card.Text>
+                <Button variant="primary">Más información</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 }
 
-function Serv(servicio,url) {
-    return(
-        <div className='my-2 py-3'>
-                    <div className='text-start h5 ps-4 ms-2'><span>{servicio}</span></div>
-                    <div className='d-flex flex-wrap px-3 justify-content-around'>
-                        <div className=''>{services('Diseño casa Athens',PR,'url')}</div>
-                        <div className=''>{services('Diseño casa Boyacá',PR,'url')}</div>
-                        <div className=''>{services('Diseño casa Patio',PR,'url')}</div>
-                    </div>
-                </div>
-    );
-}
+export default Servicios;
