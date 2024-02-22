b2d4642fd81e28671d8af16e2d13372a304f459b
AnibalGodinez <anibalgodinez64@gmail.com>
2024-02-22 10:58:16 -0600

    Navegación -Construcción

--- frontend/src/componets/Construccion.jsx
+++ frontend/src/componets/Construccion.jsx
@@ -1,33 +1,34 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
-import { Card, Container, Button } from 'react-bootstrap';
+import '../styles/Construccion.css';
 
-function Construccion() {
-  const { nombreServicio } = useParams();
-  
-  const servicios = [
-    {nombre: 'Construcción'}
-  ];
-
-  const servicio = servicios.find(servicio => servicio.nombre === nombreServicio);
-
-  if (!servicio) {
-    return <p>Servicio no encontrado</p>;
-  }
-
+const Construccion = () => {
   return (
-    <Container className="d-flex justify-content-center">
+    <div className="construccion-container">
       <h1>Construcción</h1>
-      <Card className="mb-4">
-        <Card.Img variant="top" src={servicio.imagen} />
-        <Card.Body>
-          <Card.Title>{servicio.nombre}</Card.Title>
-          <Card.Text>{servicio.descripcion}</Card.Text>
-          <Card.Text>{servicio.infoAdicional}</Card.Text>
-          <Button variant="primary">Contactar</Button>
-        </Card.Body>
-      </Card>
-    </Container>
+      <br />
+      <br />
+      <div className="construccion-section">
+        <img className="construccion-img" src="https://scontent.ftgu4-1.fna.fbcdn.net/v/t39.30808-6/329193424_905818120620867_9219122127769507762_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=bjmnhzn4iQ8AX_5dnFu&_nc_ht=scontent.ftgu4-1.fna&oh=00_AfAke21iiJOyEW4H3KCSVbArtp4p1CIAA5-WivMxOs3Z2A&oe=65DB6764" alt="Imagen de construcción 1" />
+        <div>
+          <h2>Reconstrucción de casa</h2>
+          <p>La reconstrucción de una casa implica evaluar su estado actual, planificar el diseño, obtener permisos, llevar a cabo la demolición si es necesaria, construir la nueva estructura, instalar sistemas como fontanería y electricidad, completar los acabados interiores y exteriores, y finalmente, obtener las aprobaciones necesarias antes de que la casa esté lista para habitar.</p>
+        </div>
+      </div>
+      <div className="construccion-section">
+        <img className="construccion-img" src="https://scontent.ftgu4-1.fna.fbcdn.net/v/t39.30808-6/325354154_5513428792119842_3233149423284415225_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=HzMdDqpKJqgAX_zgLoC&_nc_ht=scontent.ftgu4-1.fna&oh=00_AfAnrGkQvKpp-035Fftkl-_fnNNrKw82Frjyj4VfRUMqVw&oe=65DAC6FC" alt="Imagen de construcción 2" />
+        <div>
+          <h2>Construcción con materiales de alta calidad</h2>
+          <p>En nuestra empresa, nos comprometemos a utilizar exclusivamente materiales de alta calidad en todos nuestros proyectos de construcción. Desde la selección de los materiales hasta su instalación, nos esforzamos por garantizar la durabilidad, la seguridad y la excelencia en cada construcción. Nuestra atención meticulosa a la calidad nos permite ofrecer resultados superiores y satisfacer las expectativas de nuestros clientes en cada proyecto.</p>
+        </div>
+      </div>
+      <div className="construccion-section">
+        <img className="construccion-img" src="https://scontent.ftgu4-1.fna.fbcdn.net/v/t39.30808-6/322258460_621170030012858_8634094977683658569_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=brJv197vHagAX9NWnVl&_nc_ht=scontent.ftgu4-1.fna&oh=00_AfBqMPS2uf2ZWmd76ielA-XaXs7sQB8qdctjk-my6Gqvyg&oe=65DA8B4A" alt="Imagen de construcción 4" />
+        <div>
+          <h2>Nuestro compromiso de calidad y servicio</h2>
+          <p>Nos dedicamos a ofrecer servicios de construcción de casas y oficinas con los más altos estándares de excelencia. Nos esforzamos por superar las expectativas de nuestros clientes en cada etapa del proceso, desde el diseño hasta la entrega final. Nuestro compromiso se refleja en la calidad de los materiales que utilizamos, la atención al detalle en la mano de obra y el servicio excepcional que proporcionamos.</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- frontend/src/componets/Servicios.jsx
+++ frontend/src/componets/Servicios.jsx
@@ -6,57 +6,54 @@ function Servicios() {
   const servicios = [
     { 
       nombre: 'Construcción', 
-      descripcion: 'Construimos oficinas de alta calidad y espacios de trabajo minimalistas.', 
+      descripcion: 'Nos especializamos en la construcción de casas y oficinas de primera calidad.', 
       imagen: 'https://scontent.ftgu4-1.fna.fbcdn.net/v/t39.30808-6/324567143_853556549283661_2032805455122645712_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=NDID2dzX9gcAX_qsAqJ&_nc_ht=scontent.ftgu4-1.fna&oh=00_AfCguxyZG5UYtlOoomQCtlgoB-CmjMPFoclBo2N-BSMLrg&oe=65DBFAE6', 
-      infoAdicional: 'Información adicional...' 
     },
     { 
       nombre: 'Remodelaciones', 
-      descripcion: 'Le damos mantenimiento a tu hogar y oficina.', 
+      descripcion: 'Nos especializamos en la construcción de casas y oficinas de trabajo de primera calidad.', 
       imagen: 'https://scontent.ftgu4-1.fna.fbcdn.net/v/t39.30808-6/322331734_1335197530666565_1245614110228181443_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=EBjzGQqZgFgAX9bdaHy&_nc_ht=scontent.ftgu4-1.fna&oh=00_AfA_TAuGztruciewHAs9YaA2H8hJ3cTyeDaWXPa9CG8OrA&oe=65DBD15A', 
-      infoAdicional: 'Información adicional...' 
+
     },
     { 
       nombre: 'Diseño de Planos', 
-      descripcion: 'Diseñamos con los estilos más modernos y con los estandares arquitectonicos mas altos.', 
+      descripcion: 'Diseñamos los planos de construcción con los estilos más modernos.', 
       imagen: 'https://scubica.s3.us-west-2.amazonaws.com/scubica/cdfdac5b8e417eaaad3bd6932a2ec85d.jpg', 
-      infoAdicional: 'Información adicional...' 
     }
-    // Agrega más servicios aquí...
-];
+  ];
 
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
-const handleButtonClick = (servicio) => {
-  if (servicio.nombre === 'Construccion') {
-    navigate('/construccion');
-  } else {
-    navigate(`/servicio/${servicio.nombre}`);
-  }
-};
+  const handleButtonClick = (servicio) => {
+    if (servicio.nombre === 'Construcción') {
+      navigate('/construccion');
+    } else {
+      navigate(`/servicio/${servicio.nombre}`);
+    }
+  };
 
-return (
-  <Container>
-    <Container className="py-3 text-center">
-      <h1>Nuestros Servicios</h1>
+  return (
+    <Container>
+      <Container className="py-3 text-center">
+        <h1>Nuestros Servicios</h1>
+      </Container>
+      <Row className="d-flex justify-content-center">
+        {servicios.map((servicio, index) => (
+          <Col xs={12} sm={6} md={4} lg={3} key={index} className="d-flex justify-content-center">
+            <Card className="mb-4">
+              <Card.Img variant="top" src={servicio.imagen} />
+              <Card.Body>
+                <Card.Title>{servicio.nombre}</Card.Title>
+                <Card.Text>{servicio.descripcion}</Card.Text>
+                <Card.Text>{servicio.infoAdicional}</Card.Text>
+                <Button variant="primary" onClick={() => handleButtonClick(servicio)}>Más información</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </Container>
-    <Row className="d-flex justify-content-center">
-      {servicios.map((servicio, index) => (
-        <Col xs={12} sm={6} md={4} lg={3} key={index} className="d-flex justify-content-center">
-          <Card className="mb-4">
-            <Card.Img variant="top" src={servicio.imagen} />
-            <Card.Body>
-              <Card.Title>{servicio.nombre}</Card.Title>
-              <Card.Text>{servicio.descripcion}</Card.Text>
-              <Card.Text>{servicio.infoAdicional}</Card.Text>
-              <Button variant="primary" onClick={() => handleButtonClick(servicio)}>Más información</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row>
-  </Container>
-);
+  );
 }
 
 export default Servicios;
