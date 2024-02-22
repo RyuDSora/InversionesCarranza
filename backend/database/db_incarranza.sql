8dfe4f7978626287680a02fddcbe9ff47d36970d
Luis Matamoros <luis_mat@hotmail.es>
2024-02-21 22:10:51 -0600

    actualizacion BD

--- backend/database/db_incarranza.sql
+++ backend/database/db_incarranza.sql
@@ -7,28 +7,120 @@ FLUSH PRIVILEGES;
 
 use db_incarranza;
 
-create table Usuarios (
-	id int not null auto_increment primary key,
-    rol int not null, 
-    nombre varchar(45) not null,
-    apellido varchar(45) not null,
-    correo varchar(100) not null,
-    telefono varchar(15) not null,
-    contasenia varchar(45) not null,
-    fechaNacimiento date not null,
-    createdAt datetime DEFAULT CURRENT_TIMESTAMP,
-    updatedAt datetime DEFAULT CURRENT_TIMESTAMP
-);
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-CREATE TABLE ProyectosRealizados (
-    id int not null auto_increment primary key,
-    servicios int not null, -- 1 = Servicio de construcción, 2 = Servicio de remodelación, 3 = Servicio de creación de planos
-    titulo varchar(500) not null,
-    descripcion text not null,
-    imagen varchar(255) not null,
-    createdAt datetime DEFAULT CURRENT_TIMESTAMP,
-    updatedAt datetime DEFAULT CURRENT_TIMESTAMP
-);
+
+
+-- -----------------------------------------------------
+-- Table `imagenes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `imagenes` (
+  `idimagenes` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  `descricion` VARCHAR(45) NOT NULL,
+  `tipo` VARCHAR(5) NOT NULL,
+  `archivo` BLOB NOT NULL,
+  PRIMARY KEY (`idimagenes`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `usuarios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `rol` INT NOT NULL,
+  `nombre` VARCHAR(45) NOT NULL,
+  `apellido` VARCHAR(45) NOT NULL,
+  `correo` VARCHAR(100) NOT NULL,
+  `telefono` VARCHAR(15) NOT NULL,
+  `contasenia` VARCHAR(45) NOT NULL,
+  `fechaNacimiento` DATETIME NOT NULL,
+  `createdAt` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  `perrfil` INT NULL,
+  `fondo` INT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_usuarios_imagenes1_idx` (`perrfil` ASC) VISIBLE,
+  INDEX `fk_usuarios_imagenes2_idx` (`fondo` ASC) VISIBLE,
+  CONSTRAINT `fk_usuarios_imagenes1`
+    FOREIGN KEY (`perrfil`)
+    REFERENCES `imagenes` (`idimagenes`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_usuarios_imagenes2`
+    FOREIGN KEY (`fondo`)
+    REFERENCES `imagenes` (`idimagenes`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `servicios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `servicios` (
+  `idservicios` INT NOT NULL AUTO_INCREMENT,
+  `nombre_servicio` VARCHAR(45) NOT NULL,
+  `detalle_servicio` VARCHAR(45) NOT NULL,
+  `servicio_padre` INT NOT NULL,
+  PRIMARY KEY (`idservicios`),
+  INDEX `fk_servicios_servicios_idx` (`servicio_padre` ASC) VISIBLE,
+  CONSTRAINT `fk_servicios_servicios`
+    FOREIGN KEY (`servicio_padre`)
+    REFERENCES `servicios` (`idservicios`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`proyectos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `proyectos` (
+  `idproyectos` INT NOT NULL AUTO_INCREMENT,
+  `nombreProyecto` VARCHAR(45) NOT NULL,
+  `descripcion_proyecto` VARCHAR(45) NOT NULL,
+  `img_principal` BLOB NOT NULL,
+  `categoria_servicio` INT NOT NULL,
+  PRIMARY KEY (`idproyectos`),
+  INDEX `fk_proyectos_servicios1_idx` (`categoria_servicio` ASC) VISIBLE,
+  CONSTRAINT `fk_proyectos_servicios1`
+    FOREIGN KEY (`categoria_servicio`)
+    REFERENCES `servicios` (`idservicios`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `proyectos_has_imagenes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `proyectos_has_imagenes` (
+  `idproyecto` INT NOT NULL,
+  `idimagen` INT NOT NULL,
+  `id` INT NOT NULL,
+  INDEX `fk_proyectos_has_imagenes_imagenes1_idx` (`idimagen` ASC) VISIBLE,
+  INDEX `fk_proyectos_has_imagenes_proyectos1_idx` (`idproyecto` ASC) VISIBLE,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_proyectos_has_imagenes_proyectos1`
+    FOREIGN KEY (`idproyecto`)
+    REFERENCES `proyectos` (`idproyectos`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_proyectos_has_imagenes_imagenes1`
+    FOREIGN KEY (`idimagen`)
+    REFERENCES `imagenes` (`idimagenes`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 insert into usuarios (rol,nombre, apellido, telefono,correo, contasenia, fechaNacimiento) values 
 (1,"Eduar","Carranza","11111111","correo_admin@incarranza.com","principal","1992-09-22"), 
