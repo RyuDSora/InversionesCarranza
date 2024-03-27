ffc896f6ba33352845fcd5fec577be2b995f604f
AnibalGodinez <anibalgodinez64@gmail.com>
2024-03-27 14:42:27 -0600

    Muestra el nombre del servicio y nombre del estado de la SOLICITUD, edita y borra.

--- frontend/src/componets/EditServiciosCliente.jsx
+++ frontend/src/componets/EditServiciosCliente.jsx
@@ -1,144 +1,146 @@
 import React, { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { Container } from 'react-bootstrap';
+import { URISolicitudes, URIEstados, URIServicios } from './Urls';
 
-const EditServiciosCliente = () => {
-    const initialFormState = { id: null, categoria_servicio: '', descripcion_solicitud: '', id_estado: '' };
-
+function EditServiciosCliente() {
     const [solicitudes, setSolicitudes] = useState([]);
-    const [formData, setFormData] = useState(initialFormState);
-    const [editingId, setEditingId] = useState(null);
+    const [estados, setEstados] = useState({});
+    const [servicios, setServicios] = useState({});
+    const [usuarioId, setUsuarioId] = useState(null); // ID del usuario actual
+    const [editandoSolicitud, setEditandoSolicitud] = useState(null); // Solicitud que se está editando
 
     useEffect(() => {
-        const loadSolicitudes = async () => {
-            try {
-                const response = await fetch('http://' + window.location.hostname + ':8000/solicitudes/');
-                const data = await response.json();
-                setSolicitudes(data);
-            } catch (error) {
-                console.error('Error:', error);
-                alert('Ocurrió un error al cargar las solicitudes. Por favor, revisa la consola para más detalles.');
-            }
-        };
-
-        loadSolicitudes();
+        // Obtener el ID del usuario actual al cargar el componente
+        const usuarioId = obtenerUsuarioId();
+        setUsuarioId(usuarioId);
     }, []);
 
-    const handleEditClick = (id, estado) => {
-        const solicitudToEdit = solicitudes.find((solicitud) => solicitud.id === id);
-        if (solicitudToEdit) {
-            setFormData(solicitudToEdit);
-            setEditingId(id);
+    useEffect(() => {
+        if (usuarioId) {
+            fetchSolicitudesPorUsuario(usuarioId);
         }
+        fetchEstados();
+        fetchServicios();
+    }, [usuarioId]);
+
+    const obtenerUsuarioId = () => {
+        // Lógica para obtener el ID del usuario actual, por ejemplo, desde el token JWT
+        // Aquí se simula retornando un ID de usuario fijo
+        return 123; // Supongamos que el ID del usuario actual es 123
     };
 
-    const handleDeleteClick = async (id, estado) => {
+    const fetchSolicitudesPorUsuario = async (userId) => {
         try {
-            await fetch('http://' + window.location.hostname + ':8000/solicitudes/' + id, {
-                method: 'DELETE',
-            });
-
-            setSolicitudes(solicitudes.filter((solicitud) => solicitud.id !== id));
+            const response = await axios.get(`${URISolicitudes}?id_usuario=${userId}`);
+            setSolicitudes(response.data);
         } catch (error) {
-            console.error('Error:', error);
-            alert('Ocurrió un error al borrar la solicitud. Por favor, revisa la consola para más detalles.');
+            console.error('Error fetching requests:', error);
         }
     };
 
-    const handleSaveClick = async (id) => {
+    const fetchEstados = async () => {
         try {
-            const response = await fetch('http://' + window.location.hostname + ':8000/solicitudes/' + id, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(formData),
+            const response = await axios.get(URIEstados);
+            const estadosData = {};
+            response.data.forEach(estado => {
+                estadosData[estado.id] = estado.nombre_estado;
             });
-    
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-    
-            const updatedData = await response.json();
-    
-            // Actualizar la lista de solicitudes en el estado
-            setSolicitudes(prevSolicitudes => prevSolicitudes.map((solicitud) => (solicitud.id === id ? { ...solicitud, ...updatedData } : solicitud)));
-    
-            // Restablecer el ID de edición y los datos del formulario
-            setEditingId(null);
-            setFormData(initialFormState);
-    
-            // Recargar las solicitudes para obtener la lista actualizada
-            const reloadResponse = await fetch('http://' + window.location.hostname + ':8000/solicitudes/');
-            const newData = await reloadResponse.json();
-            setSolicitudes(newData);
+            setEstados(estadosData);
         } catch (error) {
-            console.error('Error:', error);
-            alert('Ocurrió un error al guardar los cambios. Por favor, revisa la consola para más detalles.');
+            console.error('Error fetching states:', error);
         }
     };
-    
+
+    const fetchServicios = async () => {
+        try {
+            const response = await axios.get(URIServicios);
+            const serviciosData = {};
+            response.data.forEach(servicio => {
+                serviciosData[servicio.id] = servicio.nombre_servicio;
+            });
+            setServicios(serviciosData);
+        } catch (error) {
+            console.error('Error fetching services:', error);
+        }
+    };
+
+    const handleEditClick = (solicitudId, estadoId) => {
+        const solicitud = solicitudes.find(solicitud => solicitud.id === solicitudId);
+        setEditandoSolicitud(solicitud);
+    };
+
+    const handleUpdateClick = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.put(`${URISolicitudes}/${editandoSolicitud.id}`, editandoSolicitud);
+            fetchSolicitudesPorUsuario(usuarioId); // Refrescar las solicitudes
+            setEditandoSolicitud(null); // Salir del modo de edición
+        } catch (error) {
+            console.error('Error al actualizar la solicitud:', error);
+        }
+    };
+
+    const handleDeleteClick = async (solicitudId) => {
+        try {
+            await axios.delete(`${URISolicitudes}/${solicitudId}`);
+            fetchSolicitudesPorUsuario(usuarioId); // Refrescar las solicitudes
+        } catch (error) {
+            console.error('Error al eliminar la solicitud:', error);
+        }
+    };
+
     return (
-        <div className="container mt-5">
+        <Container className="mt-5">
             <h2 className="text-center mb-4">Solicitudes de Servicio</h2>
+            {editandoSolicitud && (
+                <form onSubmit={handleUpdateClick}>
+                    <label>
+                        Servicio:
+                        <select value={editandoSolicitud.categoria_servicio} onChange={e => setEditandoSolicitud({...editandoSolicitud, categoria_servicio: e.target.value})}>
+                            {Object.entries(servicios).map(([id, nombre]) => (
+                                <option value={id} key={id}>{nombre}</option>
+                            ))}
+                        </select>
+                    </label>
+                    <label>
+                        Descripción de solicitud:
+                        <input type="text" value={editandoSolicitud.descripcion_solicitud} onChange={e => setEditandoSolicitud({...editandoSolicitud, descripcion_solicitud: e.target.value})} />
+                    </label>
+                    <button type="submit">Actualizar</button>
+                </form>
+            )}
             <table className="table table-striped">
                 <thead>
                     <tr>
                         <th>ID</th>
-                        <th>Categoria Servicio</th>
-                        <th>Descripcion Solicitud</th>
+                        <th>Servicio</th>
+                        <th>Descripcion de la solicitud</th>
                         <th>Estado</th>
                         <th>Acciones</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {solicitudes.map((solicitud) => (
+                    {solicitudes.map(solicitud => (
                         <tr key={solicitud.id}>
                             <td>{solicitud.id}</td>
+                            <td>{servicios[solicitud.categoria_servicio]}</td>
+                            <td>{solicitud.descripcion_solicitud}</td>
+                            <td>{estados[solicitud.id_estado]}</td>
                             <td>
-                                {editingId === solicitud.id ? (
-                                    <input
-                                        type="text"
-                                        value={formData.categoria_servicio}
-                                        onChange={(e) => setFormData({ ...formData, categoria_servicio: e.target.value })}
-                                    />
-                                ) : (
-                                    solicitud.categoria_servicio
-                                )}
-                            </td>
-                            <td>
-                                {editingId === solicitud.id ? (
-                                    <input
-                                        type="text"
-                                        value={formData.descripcion_solicitud}
-                                        onChange={(e) => setFormData({ ...formData, descripcion_solicitud: e.target.value })}
-                                    />
-                                ) : (
-                                    solicitud.descripcion_solicitud
-                                )}
-                            </td>
-                            <td>{solicitud.id_estado}</td> {/* Aquí se muestra el estado sin posibilidad de edición */}
-                            <td>
-                                {editingId === solicitud.id ? (
-                                    <button className="btn btn-success" onClick={() => handleSaveClick(solicitud.id)}>
-                                        Guardar
-                                    </button>
-                                ) : (
-                                    <>
-                                        <button className="btn btn-primary mr-2" onClick={() => handleEditClick(solicitud.id, solicitud.id_estado)} disabled={solicitud.id_estado !== 1}>
-                                            Editar
-                                        </button>
-                                        <button className="btn btn-danger mx-2" onClick={() => handleDeleteClick(solicitud.id, solicitud.id_estado)} disabled={solicitud.id_estado !== 1}>
-                                            Borrar
-                                        </button>
-                                    </>
-                                )}
+                                <button className="btn btn-primary mr-2" onClick={() => handleEditClick(solicitud.id, solicitud.id_estado)} disabled={solicitud.id_estado !== 1}>
+                                    Editar
+                                </button>
+                                <button className="btn btn-danger mx-2" onClick={() => handleDeleteClick(solicitud.id, solicitud.id_estado)} disabled={solicitud.id_estado !== 1}>
+                                    Borrar
+                                </button>
                             </td>
                         </tr>
                     ))}
                 </tbody>
             </table>
-        </div>
+        </Container>
     );
-};
+}
 
-export default EditServiciosCliente; 
+export default EditServiciosCliente;
