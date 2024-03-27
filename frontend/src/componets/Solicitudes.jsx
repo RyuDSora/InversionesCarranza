320b0588c1c1cc6f76934064fc1ab6b03a4ea3d6
OmarCarranza15 <79736446+OmarCarranza15@users.noreply.github.com>
2024-03-27 12:21:05 -0600

    al realizar la solicitud te dirige a mis solicitudes

--- frontend/src/componets/Solicitudes.jsx
+++ frontend/src/componets/Solicitudes.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Modal, Form, Button, Alert } from 'react-bootstrap';
+import { Form, Button, Alert } from 'react-bootstrap';
 import axios from 'axios';
 import { URISolicitudes, URIServicios } from './Urls'; // Importa la URI para las solicitudes y los servicios
 
@@ -42,43 +42,40 @@ function Solicitud({ userId }) {
             // Mostrar mensaje de éxito
             alert('La solicitud se ha realizado con éxito.');
             // Cerrar la modal
+            window.location.href = `/EditServiciosCliente`// Recargar la página para "cerrar" la modal
         } catch (error) {
             console.error('Error al enviar la solicitud:', error);
         }
     };
 
     return (
-        <>
-            <Modal.Body>
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group className="mb-3" controlId="formBasicSelect">
-                        <Form.Label>Servicio</Form.Label>
-                        <Form.Select onChange={(e) => { setServicioSeleccionado(e.target.value) }} value={servicioSeleccionado}>
-                            <option value="">Seleccione un servicio</option>
-                            {servicios.map((servicio) => (
-                                <option key={servicio.id} value={servicio.id}>{servicio.nombre_servicio}</option>
-                            ))}
-                        </Form.Select>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label>Detalles</Form.Label>
-                        <Form.Control as="textarea" rows={3}
-                            placeholder="Ingrese la descripción del proyecto" value={descripcionSolicitud}
-                            onChange={(e) => setDescripcionSolicitud(e.target.value)} />
-                    </Form.Group>
-                    {showErrorMessage && ( // Mostrar mensaje de error solo cuando showErrorMessage sea true
-                        <Alert variant="danger" className="mt-3">
-                            Por favor, complete todos los campos.
-                        </Alert>
-                    )}
-                    <div className='w-50 m-auto d-flex justify-content-around mt-2'>
-                        <Button variant="success" type="submit">
-                            Guardar
-                        </Button>
-                    </div>
-                </Form>
-            </Modal.Body>
-    </>
+        <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicSelect">
+                <Form.Label>Servicio</Form.Label>
+                <Form.Select onChange={(e) => { setServicioSeleccionado(e.target.value) }} value={servicioSeleccionado}>
+                    <option value="">Seleccione un servicio</option>
+                    {servicios.map((servicio) => (
+                        <option key={servicio.id} value={servicio.id}>{servicio.nombre_servicio}</option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Detalles</Form.Label>
+                <Form.Control as="textarea" rows={3}
+                    placeholder="Ingrese la descripción del proyecto" value={descripcionSolicitud}
+                    onChange={(e) => setDescripcionSolicitud(e.target.value)} />
+            </Form.Group>
+            {showErrorMessage && ( // Mostrar mensaje de error solo cuando showErrorMessage sea true
+                <Alert variant="danger" className="mt-3">
+                    Por favor, complete todos los campos.
+                </Alert>
+            )}
+            <div className='w-50 m-auto d-flex justify-content-around mt-2'>
+                <Button variant="success" type="submit">
+                    Guardar
+                </Button>
+            </div>
+        </Form>
     );
 }
 
