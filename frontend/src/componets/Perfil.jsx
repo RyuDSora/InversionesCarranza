a66889b33feee253c41d421d465972d474d92ab7
AnibalGodinez <anibalgodinez64@gmail.com>
2024-04-15 02:00:10 -0600

    Foto de perfil y fondo | Actualizada

--- frontend/src/componets/Perfil.jsx
+++ frontend/src/componets/Perfil.jsx
@@ -1,85 +1,124 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {Spinner, Container} from 'react-bootstrap';
-import { useParams } from 'react-router-dom'; // Importa el hook useParams para obtener los parámetros de la URL
-import { Link,useNavigate } from 'react-router-dom';
+import { Spinner, Container, Button } from 'react-bootstrap';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 import { URIUsuarios } from "./Urls.jsx";
-import { encryptionKey,decryptValue } from "./hashes.jsx";
+import { encryptionKey, decryptValue } from "./hashes.jsx";
 import Cookies from 'js-cookie';
-
 import BGImg from '../imgs/bgimg.jpg';
 import Iperfil from '../imgs/perfil.png';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
 
 export default function Perfil() {
   const navigate = useNavigate();
-  const { userId } = useParams(); // Obtiene el userId de los parámetros de la URL
+  const { userId } = useParams();
   const [usuario, setUsuario] = useState(null);
+  const [profileImage, setProfileImage] = useState(null);
+  const [backgroundImage, setBackgroundImage] = useState(null);
+  const [showEditPerfilIcon, setShowEditPerfilIcon] = useState(false);
+  const [showEditFondoIcon, setShowEditFondoIcon] = useState(false);
   var IsPerfil = false;
-
-  
 
   useEffect(() => {
     const fetchUsuarioData = async () => {
-      
       try {
-        
-        // Realiza una solicitud GET para obtener la información del usuario desde la base de datos utilizando el userId obtenido de la URL
-        const response = await axios.get(URIUsuarios+userId);
+        const response = await axios.get(URIUsuarios + userId);
         setUsuario(response.data);
+        const fotoPerfilLocalStorage = localStorage.getItem(`fotoPerfil_${userId}`);
+        const fotoFondoLocalStorage = localStorage.getItem(`fotoFondo_${userId}`);
+        if (fotoPerfilLocalStorage) {
+          setProfileImage(fotoPerfilLocalStorage);
+        }
+        if (fotoFondoLocalStorage) {
+          setBackgroundImage(fotoFondoLocalStorage);
+        }
       } catch (error) {
         console.error("Error fetching user data:", error);
       }
     };
 
     fetchUsuarioData();
-  }, [userId]); // Asegúrate de incluir userId en la lista de dependencias para que se vuelva a cargar cuando cambie
+  }, [userId]);
+
+  const handleEditarFoto = () => {
+    setShowEditPerfilIcon(true);
+    setShowEditFondoIcon(true);
+  };
+
+  const handleFileChange = (event, type) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const imageUrl = reader.result;
+      if (type === 'profile') {
+        setProfileImage(imageUrl);
+        localStorage.setItem(`fotoPerfil_${userId}`, imageUrl); // Guarda la URL de la imagen de perfil en localStorage
+        setShowEditPerfilIcon(false); // Oculta el icono de edición de perfil
+      } else if (type === 'background') {
+        setBackgroundImage(imageUrl);
+        localStorage.setItem(`fotoFondo_${userId}`, imageUrl); // Guarda la URL de la imagen de fondo en localStorage
+        setShowEditFondoIcon(false); // Oculta el icono de edición de fondo
+      }
+    };
+    reader.readAsDataURL(file);
+  };
 
   if (!usuario) {
-    return <div>
-      Cargando información del usuario...
-      <br />
-      <Spinner animation="border" variant="primary" />
-      </div>;
+    return (
+      <div>
+        Cargando información del usuario...
+        <br />
+        <Spinner animation="border" variant="primary" />
+      </div>
+    );
   }
-  
 
-   // Formatear la fecha de nacimiento para eliminar la parte de la hora
-   const fechaNacimientoSinHora = new Date(usuario.fechaNacimiento).toISOString().split('T')[0];
-
+  const fechaNacimientoSinHora = new Date(usuario.fechaNacimiento).toISOString().split('T')[0];
 
   const handleCambiarContrasenia = () => {
-    // Aquí puedes obtener el email y el nombre del usuario
     const email = usuario.correo;
     const nombre = usuario.nombre;
-  
-    // Redirigir a la página CambiarContrasenia con los parámetros en la URL
+
     navigate(`/CambiarContrasenia?correo=${email}&nombre=${nombre}`);
   };
-  if(Cookies.get('session')){
-    if(+decryptValue(Cookies.get('UserId'), encryptionKey)===usuario.id){
-      IsPerfil=true
+
+  if (Cookies.get('session')) {
+    if (+decryptValue(Cookies.get('UserId'), encryptionKey) === usuario.id) {
+      IsPerfil = true;
     }
   }
-  
+
   return (
     <Container className="my-2">
-      <div className="border rounded-3 bg-light">
+      <div className="border rounded-3 bg-light position-relative">
         <div id="header_perfil">
           <div style={{
             width: '100%',
-            backgroundImage: 'url(' + BGImg + ')',
-            height: '150px', backgroundSize: 'cover',
+            backgroundImage: `url(${backgroundImage ? backgroundImage : usuario.fotoFondo ? usuario.fotoFondo : BGImg})`,
+            height: '150px',
+            backgroundSize: 'cover',
             backgroundPosition: 'center'
           }}
             className="rounded-top-3 bg-light">
           </div>
 
           <div className="bg-light">
-            <img ref={(node) => { node && node.style.setProperty('margin-top', '-50px', 'important') }} src={Iperfil} alt="perfil" style={{ width: '150px' }} className="bg-light rounded-circle p-1 m-1" id="img-perf1" />
+            <input type="file" onChange={(e) => handleFileChange(e, 'profile')} style={{ display: 'none' }} />
+            <img onClick={() => document.querySelector('input[type="file"]').click()} src={profileImage ? profileImage : usuario.fotoPerfil ? usuario.fotoPerfil : Iperfil} alt="perfil" style={{ width: '150px' }} className="bg-light rounded-circle p-1 m-1" id="img-perf1" />
+            {showEditPerfilIcon && (
+              <Button variant="link" onClick={() => document.querySelector('input[type="file"]').click()} className="edit-icon"><FontAwesomeIcon icon={faEdit} /></Button>
+            )}
+          </div>
+          <div className="bg-light position-absolute start-0 top-0">
+            {showEditFondoIcon && (
+              <div>
+                <input type="file" onChange={(e) => handleFileChange(e, 'background')} style={{ display: 'none' }} />
+                <Button variant="link" onClick={() => document.querySelectorAll('input[type="file"]')[1].click()} className="edit-fondo-icon"><FontAwesomeIcon icon={faEdit} /></Button>
+              </div>
+            )}
           </div>
           <span className="h2">Mi Perfil</span>
-          <div></div>
         </div>
         <hr />
         <div id="body_oerfil" className="px-3">
@@ -112,21 +151,11 @@ export default function Perfil() {
         </div>
         <hr />
         <div id="foot_perfil" style={IsPerfil ? { display: 'block'}:{display:'none' }} >
-          <Link to={`/EditarFoto/${userId}`} className="btn btn-primary me-2">Editar Foto</Link>
           <Link to={`/EditarPerfil/${userId}`} className="btn btn-primary me-2">Editar Perfil</Link>
-          <button onClick={handleCambiarContrasenia} className="btn btn-primary me-2">Cambiar Contraseña</button>
+          <button onClick={handleCambiarContrasenia} className="btn btn-danger me-2">Cambiar Contraseña</button>
+          <button onClick={handleEditarFoto} className="btn btn-primary">Editar Foto</button>
         </div>
-
-        <br />
       </div>
-      <br />
     </Container>
   );
 }
-
-//al momento de dar click al boton editar perfil, se activan todos los inputs, excepto correo.
-//ademas el input de fecha de nacimiento cambia de text->date, 
-//el de genero deberia de cambiar o bien a un select o au check/radio con las opciones M,F,N/A
-//se agrega un nuevo input bajo el de password para la confirmacion de contraseña
-//se agrega un input file al lado de la img de fondo, y al lado de img de perfil
-//y por ultimo el boton editar perfil cambiaria por un guardar y otro cancelar
